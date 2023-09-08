a9290c0a655eea59ba2ccaaf27adbe123bdde2ce
hankk <hankk20@gmail.com>
2023-09-08 17:15:50 +0900

    JWKSet Bean으로 관리  - JWTCotroller에서 요청시 마다 비효율적으로 JWKSet을 생성하여 Spring에서 SingleTone으로 관리하게 Bean으로 설정

--- auth/src/main/java/kr/co/kindernoti/auth/configuration/JwtConfiguration.java
+++ auth/src/main/java/kr/co/kindernoti/auth/configuration/JwtConfiguration.java
@@ -2,6 +2,8 @@ package kr.co.kindernoti.auth.configuration;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JwtConfiguration {
 
+    /**
+     * JWK 개인키
+     * parseFormPEMEncodeObjects 메소드를 사용 하기위해 bouncycastle 라이브러리가 필요함
+     * @param jwtProperties
+     * @return
+     * @throws JOSEException
+     */
     @Bean
     public RSAKey rsaKey(JwtProperties jwtProperties) throws JOSEException {
         return JWK.parseFromPEMEncodedObjects(jwtProperties.readPrivateKey())
                 .toRSAKey();
+    }
+
+    /**
+     * JWK 공개키
+     * @param rsaKey
+     * @return
+     * @throws JOSEException
+     */
+    @Bean
+    public JWKSet publicRsaKey(RSAKey rsaKey) throws JOSEException {
+        RSAKey publicKey = new RSAKey.Builder(rsaKey.toRSAPublicKey())
+                .keyUse(KeyUse.SIGNATURE)
+                .build();
+        return new JWKSet(publicKey.toPublicJWK());
     }
 }
--- auth/src/main/java/kr/co/kindernoti/auth/security/jwt/JwtController.java
+++ auth/src/main/java/kr/co/kindernoti/auth/security/jwt/JwtController.java
@@ -2,9 +2,8 @@ package kr.co.kindernoti.auth.security.jwt;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.KeyUse;
-import com.nimbusds.jose.jwk.RSAKey;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -15,14 +14,10 @@ import java.util.Map;
 @RestController
 public class JwtController {
 
-    private final RSAKey rsaKey;
+    private final JWKSet jwkSet;
 
     @GetMapping("/.well-known/jwks.json")
-    public Mono<Map<String, Object>> wellKnownJwk() throws JOSEException {
-        RSAKey build = new RSAKey.Builder(rsaKey.toRSAPublicKey())
-                .keyUse(KeyUse.SIGNATURE)
-                .build();
-        Map<String, Object> jsonObject = new JWKSet(build.toPublicJWK()).toJSONObject();
-        return Mono.just(jsonObject);
+    public Mono<ResponseEntity<Map<String,Object>>> wellKnownJwk() throws JOSEException {
+        return Mono.just(ResponseEntity.ok(jwkSet.toJSONObject()));
     }
 }
