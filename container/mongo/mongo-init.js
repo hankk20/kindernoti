636ffb584d3a74663505e038a59795dda97ee0eb
hankk <hankk20@gmail.com>
2023-10-16 16:29:42 +0900

    mongodb 컨테이너 초기화 스크립트 외부 환경변수 적용

--- container/mongo/mongo-init.js
+++ container/mongo/mongo-init.js
@@ -1,17 +1,22 @@
 db = db.getSiblingDB("auth")
 db.createUser({
-  user: "authuser",
-  pwd: "authuser11",
+  user: process.env.SERVICE_AUTH_DB_USER,
+  pwd: process.env.SERVICE_AUTH_DB_PASSWORD,
   roles: [{ role: "readWrite", db: "auth" }],
 });
-db.createCollection("users")
-db.users.createIndex({userId:1, oauthProvider:1})
+db.createCollection("user")
+db.user.createIndex({userId:1, oauthProvider:1})
 
 db = db.getSiblingDB("parent")
-
 db.createUser({
-  user: "parentuser",
-  pwd: "parentuser11",
+  user: process.env.SERVICE_PARENT_DB_USER,
+  pwd: process.env.SERVICE_PARENT_DB_PASSWORD,
   roles: [{ role: "readWrite", db: "parent" }],
 });
-db.createCollection("users")
+
+db = db.getSiblingDB("teacher")
+db.createUser({
+  user: process.env.SERVICE_TEACHER_DB_USER,
+  pwd: process.env.SERVICE_TEACHER_DB_PASSWORD,
+  roles: [{ role: "readWrite", db: "teacher" }],
+});
